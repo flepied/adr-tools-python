31563ebaaf40bbd39ba22645b28694d1d80ebd80
Victor <vsluiter@yahoo.com>
2019-05-25 23:57:09 +0200

    Added comment

--- src/adr_config.py
+++ src/adr_config.py
@@ -1,9 +1,16 @@
 import os 
 
+# Original bash implementation generates strings with paths to
+# bin and template dir (both the same).
+
+# In this python implementation I've changed this to a 
+# dictionary. I think this is more future proof and way
+# more 'pythonic' and less error prone.
+
+
+
 def adr_config():
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    #bin_dir=("adr_bin_dir=\'" + dir_path + "\'")
-    #template_dir=("adr_template_dir=\"" + dir_path + "\"")
     config = {"bin_dir"     : dir_path ,
               "template_dir": dir_path 
     }
@@ -12,5 +19,6 @@ def adr_config():
 if __name__ == "__main__":
     import sys
     config = adr_config()
+    # print output if run standalone
     for key,val in config.items():
         print(key,"=\"",val,"\"", sep='')
