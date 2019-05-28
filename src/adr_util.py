78c44f25c7fffa346f63c97fe7a2c575297824ff
Victor <vsluiter@yahoo.com>
2019-05-29 00:02:02 +0200

    adr-new now almost working, files are correctly named and timestamped

--- src/adr_util.py
+++ src/adr_util.py
@@ -1,6 +1,8 @@
 import os
+import fileinput
 # To copy files
 from shutil import copyfile
+from datetime import date
 
 # adr-config:
 # Original bash implementation generates strings with paths to
@@ -34,7 +36,7 @@ def adr_init(config, localpath, dirname):
         print ("Creation of the directory %s failed" % dirname)
     else:  
         print ("Successfully created the directory %s" % dirname)
-        adr_new(config, localpath, dirname, 'record-architecture-decisions')
+        adr_new(config, localpath, 'record-architecture-decisions')
     return(0)
 
 # This function is used to read the .adr-dir file (written in adr_init), to determine the relative path for the 
@@ -42,16 +44,113 @@ def adr_init(config, localpath, dirname):
 def find_alternate_dir():
     directory = 'doc/adr/'
     try:
+        # open local file
         fh = open('.adr-dir', 'r')
-        directory = fh.read()
+        # add slash to remain compatible with 'default' /doc/adr/
+        directory = fh.read()+'/'
     except FileNotFoundError:
         None
     return directory
 
+def adr_find_index(adr_dir):
+    # find all files in a directory
+    # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+    from os import listdir
+    from os.path import isfile, join
+    
+    adr_index = dict();
+    # start with index is zero, will be incremented after search
+    adr_index['n'] = 0
+    adr_index['text'] = '0000'
+    adr_index['adr_list'] = list()
+
+    onlyfiles = [f for f in listdir(adr_dir) if isfile(join(adr_dir, f))]
+    # search for highest adr number
+    for file in onlyfiles:
+        try:
+            adr_index['adr_list'].append(file)
+            #print(type(file))
+            # get number by reading first 4 characters
+            number = int(file[:4])
+            #print(number)
+            # increase index if higher number is found
+            if (number > adr_index['n']):
+                adr_index['n'] = number
+        except: 
+            print (file + " is not a valid ADR filename")
+            None
+    adr_index['n'] += 1
+
+    # Format number to string with 4 characters
+    # https://stackoverflow.com/questions/11714859/how-to-display-the-first-few-characters-of-a-string-in-python
+
+    adr_index['text'] = '{0:04d}'.format(adr_index['n'])
+    #print(onlyfiles)
+    print(adr_index['adr_list'])
+    print("new adr_index is " + adr_index['text'])
+    return adr_index
+
 # adr-new
-def adr_new(config, localpath, dirname, title):
+def adr_new(config, localpath, title):
+    # start with no error; if it changes along the way, we have an error
+
+    result = 'no error'
+
+    # directory for the template
     src= config["adr_template_dir"]+'/template.md'
-    dst = localpath+'/'+dirname + 'template.md'
-    print(src + ' ' + dst)
-    copyfile(src, dst)
-    return(0)
+    
+    #check input argument for the ADR title
+    try:
+        # check if title can be converted to string, and 
+        # replace spaces with dashes on the go
+        print(title)
+        
+        if type(title) == list:
+            title_checked = "-".join(title).replace(' ','-')
+        elif type(title) == str:
+            title_checked = (title).replace(' ','-')
+    except ValueError:
+        result = 'Title was no string'
+        print ("adr-new had no valid input for the title")
+    
+    if (result == 'no error' ):
+        # location of adrs
+        adr_dir = localpath+'/'+ find_alternate_dir()
+        # find highest index
+        adr_index  = adr_find_index(adr_dir)
+        # combine data to make destination path
+        dst =  adr_dir + adr_index['text'] + '-' + title_checked + '.md'
+        #print(src + ' ' + dst)
+        # copy template to destination directory, with correct title
+        copyfile(src, dst)
+        adr_write_number_and_header(dst, adr_index, title_checked)
+        #adr_write_header_title(dst, title_checked)
+        #adr_write_date(dst, date)
+        #adr_write_status(dst,status)
+    return(result)
+
+# Write ADR number in filename and header
+def adr_write_number_and_header(dst,adr_index,adr_title=None):
+    test=''
+    # open file for appending and reading and replacing
+    # https://kaijento.github.io/2017/05/28/python-replacing-lines-in-file/
+    for line in fileinput.input(dst, inplace=True):
+        if fileinput.filelineno() == 1:
+            test = line
+            # first replace number
+            line = '# '+str(adr_index['n'])+'. ' + test.split('.',1)[1]
+            # now add title if needed
+            if (adr_title != None):
+                line = line.split('.',1)[0] + '. ' + adr_title.replace('-',' ').title()
+            print(line,end='\n')
+        elif fileinput.filelineno() == 3:
+            # https://www.programiz.com/python-programming/datetime/current-datetime
+            today = date.today()
+            print('Date: ' + today.strftime("%Y-%m-%d") )
+        else:
+        #keep existing content 
+            print(line, end='')
+    fileinput.close()
+    print(test)
+            
+
